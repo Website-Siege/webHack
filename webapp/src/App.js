9615e17b77ae80258963799f4a872cd975ae5da4
bfiloteo <83046619+bfiloteo@users.noreply.github.com>
2022-10-02 18:50:12 -0700

    Add files via upload

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,22 +1,21 @@
+import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from './main';
-import Login from './routes/loginPage';
-import User from './routes/userPage';
-import Contact from './routes/contact';
-import About from './routes/aboutUs';
+import loginPage from './routes/loginPage';
+import userPage from './routes/userPage';
+import contact from './routes/contact';
+import aboutUs from './routes/aboutUs';
 
 function App() {
   return (
     //Routes for website
     <Router>
       <Routes>
-        <Route exact path="/" element={<Main />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/user" element={<User />} />
-	      <Route exact path="/about" element={<About />} />
-	      <Route exact path="/contact"  element={<Contact />} />
+        <Route exact path="/" element={<Main />}/>
+        <Route exact path="/loginPage" element={<loginPage />}/>
+        <Route exact path="/login?credentials" element={<userPage />}/>
       </Routes>
     </Router>
   );
